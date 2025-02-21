991ab814d8d4ffd57211eaef54b98afece552b43
Sammy Dian Yañez <sammydianya@gmail.com>
2025-02-21 16:30:20 +0100

    Update zapatillas.js

--- app/public/js/zapatillas.js
+++ app/public/js/zapatillas.js
@@ -1,50 +1,45 @@
+function buscarZapatilla(buscar) {
+  const busqueda = {
+    articulo: -1,
+    busqueda: buscar,
+  };
 
-function buscarZapatilla(buscar){
-    const busqueda = {
-        articulo:-1,
-        busqueda:buscar
-    }
+  const opciones = {
+    method: "POST",
+    body: JSON.stringify(busqueda),
+  };
 
-    const opciones = {
-        method: "POST",
-        body: JSON.stringify(busqueda)
-    };
-
-    fetch("API/buscadorZapatillas.php", opciones)
-    .then(resultado => resultado.json())
-    .then(datos =>{
-        zapatillas.innerHTML = "";
-        for(zapatilla of datos){
-            zapatillas.innerHTML += `
-            <article class="zapatilla">
-                <img src="public/img/zapatillas/${zapatilla["imagen"]}" alt="Zapatillas Airforce color blanco">
-                <div class="detalles-producto">
-                    <h2 class="titulo-producto">${zapatilla["modelo"]}</h2>
-                    <p class="descripcion-producto">${zapatilla["descripcion"]}</p>
-                    <p class="precio-producto">${zapatilla["precio"]}</p>
-                    <a href="#" class="boton-compra" aria-label="Añadir Airforce al carrito">Añadir al Carrito</a>
-                </div>
-            </article>`;
+  fetch("API/buscadorZapatillas.php", opciones)
+    .then((resultado) => resultado.json())
+    .then((datos) => {
+      zapatillas.innerHTML = "";
+      if (datos.length > 0) {
+        for (zapatilla of datos) {
+          zapatillas.innerHTML += `
+                <article class="zapatilla" data-articuloid="${zapatilla["id"]}">
+                    <img src="public/img/zapatillas/${zapatilla["imagen"]}" alt="Zapatillas Airforce color blanco">
+                    <div class="detalles-producto">
+                        <h2 class="titulo-producto">${zapatilla["modelo"]}</h2>
+                        <p class="descripcion-producto">${zapatilla["descripcion"]}</p>
+                        <p class="precio-producto">${zapatilla["precio"]}</p>
+                        <a href="#" class="boton-compra" aria-label="Añadir Airforce al carrito">Añadir al Carrito</a>
+                    </div>
+                </article>`;
         }
-
-
+      }else{
+        zapatillas.innerHTML = "No  existen  zapatillas  con  el  texto introducido como modelo";
+      }
     });
 }
 
+window.addEventListener("DOMContentLoaded", () => {
+  const zapatillas = document.getElementById("zapatillas");
 
-window.addEventListener("DOMContentLoaded", ()=>{
+  buscarZapatilla("");
 
-    const zapatillas = document.getElementById("zapatillas");
+  const bucarZapailla = document.getElementById("buscar-zapatilla");
 
-
-    buscarZapatilla("");
-
-    const bucarZapailla = document.getElementById("buscar-zapatilla"); 
-
-    bucarZapailla.addEventListener("input",()=>{
-        buscarZapatilla(bucarZapailla.value);
-    });
-
-    
+  bucarZapailla.addEventListener("input", () => {
+    buscarZapatilla(bucarZapailla.value);
+  });
 });
-
