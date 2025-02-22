70b879ba0d6238116109187efcfa6bb570718d26
Bogdandabeast <bogdan.console@gmail.com>
2025-02-22 14:20:54 +0100

    usuariosdeprueba para bbdd

---
+++ usuariosdeprueba.sql
@@ -0,0 +1,6 @@
+INSERT INTO USUARIO (nombre, apellidos, email, clave, tipo)
+VALUES 
+    ('Juan', 'Pérez Gómez', 'juan.perez@example.com', 'claveSegura123!', 'REGISTRADO'),
+    ('Ana', 'Martínez López', 'ana.martinez@example.com', 'claveSegura456!', 'INVITADO'),
+    ('Luis', 'Rodríguez Sánchez', 'luis.rodriguez@example.com', 'claveSegura789!', 'REGISTRADO'),
+    ('María', 'García Fernández', 'maria.garcia@example.com', 'claveSeguraABC!', 'INVITADO');
